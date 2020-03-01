fa4734771b370dcfeeb24c9afddd3529dd5babdd
Gabriel Erike Andreazza <k.gabriel.andreazza@gmail.com>
2020-03-01 15:28:36 -0300

    testando outro ip

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,8 +8,8 @@ use App\Mylibs\MineStat;
 class IndexController extends Controller
 {
     public function index(){
-    $ip = '191.253.104.78';
-    $port = '25565';
+    $ip = '35.199.121.209';
+    $port = '25570';
     $ms = new MineStat($ip, $port);
     return view('server.status')->with(['status' => $ms, 'ip' => $ip, 'port' => $port]);
 
